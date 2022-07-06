da5851412684face9e69343617671801efd5491f
jpbucsd <jpburkhardt@ucsd.edu>
2022-07-06 14:59:33 -0700

    adding initial steps of RNA seq pipeline in form of bash script

---
+++ align.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#arguement 1: directory with fastq files
+#arguement 2: reference genome file (ends in .fna)
+for filename in $1/*.fq.gz do
+	tar xzvf ${filename}
+done
+
+for filename in $1/*fastq.gz do
+	tar xzvf ${filename}
+done
+
+for filename in $1/*.fq do
+	bwa mem $2 filename | samtools view -bS > ${filename}.bam
+done
+
+for filename in $1/*.fastq do
+	bwa mem $2 filename | samtools view -bS > ${filename}.bam
+done
+
+#a reference genome index may have to be created from a reference genome fasta and GTF files. this can be done with bwa index. see lab 1 from cse 185
+
+#the next step will be to use the tool rsem, which will output a customized txt file containing the expression of each gene
+#maybe a custom tool for this part based on what dr. sun wants
---
+++ aligner_tools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install gcc
+sudo apt-get install make
+sudo apt-get install libbz2-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install libncurses5-dev 
+sudo apt-get install libncursesw5-dev
+sudo apt-get install liblzma-dev
+
+sudo apt install bwa
+sudo apt install samtools
+
