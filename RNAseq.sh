6c2a970153a4dd4a54a7d4ac7a456912cfe40819
jpbucsd <93294940+jpbucsd@users.noreply.github.com>
2022-07-25 13:19:57 -0700

    Syntax Errors

--- RNAseq.sh
+++ RNAseq.sh
@@ -137,15 +137,15 @@ then
 	if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.fna ]
 	then
 		if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.fna.gz ]
-		else
+		then
 			wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz
 		fi
 		gzip -d GCF_000001405.40_GRCh38.p14_genomic.fna.gz
 	fi
 	if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.gtf ]
-	else
+	then
 		if ! [ -f GCF_000001405.40_GRCh38.p14_genomic.gtf.gz ]
-		else
+		then
 			wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gtf.gz
 		fi
 		gzip -d GCF_000001405.40_GRCh38.p14_genomic.gtf.gz
