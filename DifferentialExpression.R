f7be67bccfc62aefe93a273c6c1c3584434553f4
jpbucsd <93294940+jpbucsd@users.noreply.github.com>
2022-07-29 16:36:59 -0700

    Adding differential expression analysis with DESeq2
    the new parts of the code should work but is not tested, I also added a parameter for the directory location

--- DifferentialExpression.R
+++ DifferentialExpression.R
@@ -3,13 +3,15 @@ library("tximport")
 
 #parse command line arguments
 #command line usage 1:
-#Rscript Rtest.R -1 firstSet set1file1.genes.results set1file2.genes.results ... set1fileN.genes.results -2 secondSet set2file1.genes.results set2file2.genes.results ... set2fileN.genes.results
+#Rscript Rtest.R -1 firstSet set1file1.genes.results set1file2.genes.results ... set1fileN.genes.results -2 secondSet set2file1.genes.results set2file2.genes.results ... set2fileN.genes.results -d path/to/directory
 #command line usage 2:
-#Rscript Rtest.R -1 set1file1.genes.results set1file2.genes.results ... set1fileN.genes.results -2 set2file1.genes.results set2file2.genes.results ... set2fileN.genes.results
+#Rscript Rtest.R -1 set1file1.genes.results set1file2.genes.results ... set1fileN.genes.results -2 set2file1.genes.results set2file2.genes.results ... set2fileN.genes.results -d path/to/directory
 
 args <- commandArgs(trailingOnly = TRUE)
 first <- FALSE
 second <- FALSE
+directory <- FALSE
+dirPath <- ""
 named <- FALSE
 firstName <- ""
 secondName <- ""
@@ -24,12 +26,19 @@ for (arg in args) {
           {
             first = TRUE
             second = FALSE
+            directory = FALSE
             named = FALSE
-          } else {
+          } else if (substring(arg, first = 1, last = 2) == "-2") {
             #assume it is -2
             second = TRUE
             first = FALSE
+            directory = FALSE
             named = FALSE
+          } else if (substring(arg, first = 1, last = 2) == "-d") {
+            #assume it is -2
+            second = FALSE
+            first = FALSE
+            directory = TRUE
           }
     } else {
           if ( first )
@@ -52,7 +61,7 @@ for (arg in args) {
                   files1[files1ind] = arg
                   files1ind = files1ind + 1
              }
-          } else {
+          } else if (second) {
             #assume second
             if ( !named )
              {
@@ -62,22 +71,50 @@ for (arg in args) {
                         secondName = substring(arg, first = 0, last = nchar(arg) - 14)
                         #we also must declare the first variable
                         files2[1] = arg
-                        files2ind = files2ind + 1
+                        #files2ind = files2ind + 1
                         named = TRUE
                   } else {
                         secondName = arg
                         named = TRUE
                   }
              } else {
-                  files2[files2ind] = arg
                   files2ind = files2ind + 1
+                  files2[files2ind] = arg
              }
+          } else if (directory)
+          {
+              dirPath = arg
           }
      
     }
  }
- #check arguments
- print(firstName)
- print(files1)
- print(secondName)
- print(files2)
+
+conditions <- c(rep(firstName,files1ind),rep(secondName,files2ind))
+files <- c()
+snames <- c()
+for (file in files1) {
+    append(snames, substring(file, first = 0, last = nchar(file) - 14))
+    fname <- paste(dirPath,file,sep="/")
+    append(files,fname)
+}
+for (file in files2) {
+    append(snames, substring(file, first = 0, last = nchar(file) - 14))
+    fname <- paste(dirPath,file,sep="/")
+    append(files,fname)
+}
+samples <- data.frame("run"=snames,"condition"=conditions)
+names(files) = samples$run
+
+#convert RSEM results
+txi <- tximport(files, type = "rsem")
+txi$length[txi$length == 0] <- 1
+ddsTxi <- DESeqDataSetFromTximport(txi,coData = samples, design = ~ condition)
+
+#filtering, filter low counts to ignore them
+keep <- rowSums(counts(ddsTxi)) >= 10
+ddsTxi <- ddsTxi[keep,]
+
+#write results
+ofnname <- paste(firstName,secondName,sep="_vs_")
+write.csv(as.data.frame(res), file=ofname)
+                
