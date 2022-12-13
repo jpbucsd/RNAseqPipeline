d4be8dbf07a48c9cd0ad6f1172bfe5de8af5ad75
jpbucsd <93294940+jpbucsd@users.noreply.github.com>
2022-12-13 13:24:46 -0800

    Update splicingvsexpression.py

--- splicingvsexpression.py
+++ splicingvsexpression.py
@@ -6,3 +6,15 @@ from matplotlib import pyplot as plt
 
 #AS_events_sig_spreadsheet
 #Prog_vs_Diff.csv
+
+log2 = pd.read_csv("Prog_vs_Diff.csv")
+AS = pd.read_excel("AS_events_sig_spreadsheet.xlsx")
+
+AS['log2FoldChange'] = 0
+
+for index, row in AS.iterrows():
+  for index2, row2 in log2.iterrows():
+    if row["geneSymbol"].strip() == row2["gene_id"].strip():
+      row['log2FoldChange'] = row2["gene_id"]
+
+display(AS)
