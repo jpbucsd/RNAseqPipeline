966b142607efa0432961dc4c5d38cc1489359ae3
Jordan Burkhardt <jpburkhardt@ucsd.edu>
2022-12-13 14:09:25 -0800

    debugging

--- splicingvsexpression.py
+++ splicingvsexpression.py
@@ -8,13 +8,13 @@ from matplotlib import pyplot as plt
 #Prog_vs_Diff.csv
 
 log2 = pd.read_csv("Prog_vs_Diff.csv")
-AS = pd.read_excel("AS_events_sig_spreadsheet.xlsx")
+AS = pd.read_csv("AS_events_sig_spreadsheet.csv")
 
 AS['log2FoldChange'] = 0
 
 for index, row in AS.iterrows():
   for index2, row2 in log2.iterrows():
-    if row["geneSymbol"].strip() == row2["gene_id"].strip():
-      row['log2FoldChange'] = row2["gene_id"]
+    if row["geneSymbol"].strip() == row2[0].strip():
+      row['log2FoldChange'] = row2['log2FoldChange']
 
 display(AS)
